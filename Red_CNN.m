4d8af55f42a433af9b5808018c2cca296043f2cb
z0gSh1u <zx.cs@qq.com>
2022-11-22 17:26:39 +0800

    We need Caffe no more.

--- Red_CNN.m
+++ Red_CNN.m
@@ -1,16 +1,15 @@
-clear all;
-caffe.reset_all();
-% MODEL
-model = ['Red_CNN.prototxt'];
-weights ='Red_CNN.caffemodel';
-%Init Net
-net = caffe.Net(model,weights,'test');
+clear all; clc
+
+% Specify files to import
+protofile = 'Red_CNN.prototxt';
+datafile = 'Red_CNN.caffemodel';
+% Import network
+net = importCaffeNetwork(protofile, datafile, 'OutputLayerType', 'regression');
+% Test
 pfile = ['L506_QD_3_1.CT.0003.0035.2015.12.22.20.45.42.541197.358791561.IMA'];
 X = dicomread(pfile);
-im_input=double(X)/3000;
-input_data = {im_input};
-out_data = net.forward(input_data);
-im_output = cell2mat(out_data);
+im_input = double(X)/3000;
+im_output = net.predict(im_input);
 subplot(1,2,1);
 imshow(im_input,[850/3000 1250/3000]);
 subplot(1,2,2);
